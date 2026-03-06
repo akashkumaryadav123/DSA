e06e8442b0f29ec1a22da0c0e02ea70f790ad9b7
wellcheckedcode <akashkumar197yadav@gmail.com>
2026-03-06 16:07:08 +0530

    solved 2nd question today itself

---
+++ binary_search/findSmallestDivisor.py
@@ -0,0 +1,21 @@
+nums = [1,2,5,9]
+threshold = 6
+def samllestDivisor(nums,thershold):
+    def check(nums,divisor,threshold):
+                res=0
+                for n in nums:
+                    res+=math.ceil(n/divisor)
+                return res<=threshold
+    l=1
+    r=max(nums)
+
+    while l<=r:
+        mid=(l+r)//2
+        if  check(nums,mid,threshold):
+            r=mid-1
+        else:
+            l=mid+1
+    return l
+
+
+        
