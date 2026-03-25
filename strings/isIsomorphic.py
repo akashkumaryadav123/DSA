e6be236db4f88b566f8cb4de1b3708b7eaae6013
wellcheckedcode <akashkumar197yadav@gmail.com>
2026-03-25 09:31:35 +0530

    chat-gpt se chapa hai nahi aa rha tha

---
+++ strings/isIsomorphic.py
@@ -0,0 +1,11 @@
+def checkIsomorophicStrings(s,t):
+    s_unq=set(s)
+    t_unq=set(t)
+    if len(s_unq)==len(t_unq):
+        for i in s:
+            pass
+    else:
+        return False
+print(checkIsomorophicStrings(s="bbbaaaba",t="aaabbbba"))
+
+
